ef8011461ff4142a4cb095b34facc2c73350bd2e
Rayhanuc <rayhanctg4@gmail.com>
2022-10-09 01:25:08 +0600

    01 - Learn about CRUD (create, read, update, delete)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "main#index"
 
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/delete'
+
   match 'about', to: "main#about", via: :get
   match 'hello', to: "main#hello", via: :get
 
@@ -8,6 +14,9 @@ Rails.application.routes.draw do
   get 'tasks/index'
   get 'tasks/new'
   get 'tasks/edit'
+  get 'tasks/show'
+  get 'tasks/delete'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
